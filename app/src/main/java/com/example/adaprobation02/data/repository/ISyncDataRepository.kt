a1be91a476f82da2369c88d6bc180bdb128097e2
taeKrits <taekrits@gmail.com>
2023-08-07 11:22:12 +0700

    no message

--- app/src/main/java/com/example/adaprobation02/data/repository/ISyncDataRepository.kt
+++ app/src/main/java/com/example/adaprobation02/data/repository/ISyncDataRepository.kt
@@ -3,6 +3,8 @@ package com.example.adaprobation02.data.repository
 import com.example.adaprobation02.data.model.CDataDownloadListDB
 
 interface ISyncDataRepository {
+
+    //interface ไม่มีก็ได้ ถ้ามีจะดีในตอนทำ unit test
     fun C_GETxTaskDataList(): List<CDataDownloadListDB>
 
 }
